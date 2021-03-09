c589e0acaed669bdeab645a504ac6a4c8b4fceb9
Olivia Persson <Olivia__P@hotmail.com>
2021-03-09 13:58:11 +0100

    Person class

---
+++ Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTbooking
+{
+    class Person
+    {
+        //Properties
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        //Defaultconstructor
+        public Person()
+        {
+
+        }
+
+        //Method to register new person
+        public void Registration()
+        {
+            Console.WriteLine("Enter name: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter phone: ");
+            Phone = Console.ReadLine();
+            Console.WriteLine("Enter email: ");
+            Email = Console.ReadLine();
+        }
+    }
+}
