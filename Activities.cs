777430aa2dd7630d0be3b219ee186dd33e541c62
Basma Abdulahad <basma_812@hotmail.com>
2021-03-09 13:21:51 +0100

    update1

--- Activities.cs
+++ Activities.cs
@@ -8,6 +8,19 @@ namespace PTbooking
 {
     class Activities
     {
+        // Create a fields
+        public string selfTraining;  
+        public string groupTraining;
+        public string ptTraining;
+        public string ptConsultation;
 
+        // Create a class constructor
+        public Activities()
+        {
+            selfTraining = "Your time for self training and using the selected machines is successfully booked";
+            groupTraining = "Your time for group training is successfully booked";
+            ptTraining = "Your time for PT training is successfully booked";
+            ptConsultation = "Your time for PT Consultation is successfully booked";
+        }
     }
 }
