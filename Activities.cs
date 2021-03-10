94f90a4d0031b9e050c7472059f3bcf89e53f938
Basma Abdulahad <basma_812@hotmail.com>
2021-03-10 13:01:31 +0100

    update2

--- Activities.cs
+++ Activities.cs
@@ -5,22 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PTbooking
-{
-    class Activities
-    {
-        // Create a fields
-        public string selfTraining;  
-        public string groupTraining;
-        public string ptTraining;
-        public string ptConsultation;
+{   
 
-        // Create a class constructor
-        public Activities()
-        {
-            selfTraining = "Your time for self training and using the selected machines is successfully booked";
-            groupTraining = "Your time for group training is successfully booked";
-            ptTraining = "Your time for PT training is successfully booked";
-            ptConsultation = "Your time for PT Consultation is successfully booked";
+    public class Activities //:Person;
+    {
+        public string selfTraining;
+        public int selfTrainingMaxSize;
+        public string groupTraining;
+        public int groupTrainingMaxSize;
+        public string ptTraining;
+        public int ptTrainingMaxSize;
+        public string ptConsultation;
+        public int ptConsultationMaxSize;
+
+        private List<Person> selfTraining = new List<Person>();
+        private const int selfTrainingMaxSize = 10;
+        
+        public bool addToSelfTraining(Person p) {
+            if(selfTraining.Count >= selfTrainingMaxSize) {
+                return false;
+            }
+            selfTraining.Add(p);
+            return true;
         }
-    }
+
+        private List<Person> groupTraining = new List<Person>();
+        private const int selfTrainingMaxSize = 6;
+        
+        public bool addToGroupTraining(Person p) {
+            if(groupTraining.Count >= groupTrainingMaxSize) {
+                return false;
+            }
+            groupTraining.Add(p);
+            return true;
+        }
+
+        private List<Person> ptTraining = new List<Person>();
+        private const int ptTrainingMaxSize = 5;
+        
+        public bool addToPtTraining(Person p) {
+            if(ptTraining.Count >= ptTrainingMaxSize) {
+                return false;
+            }
+            ptTraining.Add(p);
+            return true;
+        }
+
+        private List<Person> ptConsultation = new List<Person>();
+        private const int ptConsultationMaxSize = 3;
+        
+        public bool addToPtConsultation(Person p) {
+            if(ptConsultation.Count >= ptConsultationMaxSize) {
+                return false;
+            }
+            ptConsultation.Add(p);
+            return true;
+        }
 }
